e947e9fc2eddad84b123b231e7306ae8d4adc18b
LittleFox4112 <quinelawensky@gmail.com>
2024-04-26 12:11:59 +0700

    File dan aset tes transisi scene kamar 1

---
+++ Transisi Kamar.swift
@@ -0,0 +1,18 @@
+//
+//  Transisi Kamar.swift
+//  Loti
+//
+//  Created by Quinela Wensky on 26/04/24.
+//
+
+import SwiftUI
+
+struct Transisi_Kamar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Transisi_Kamar()
+}
