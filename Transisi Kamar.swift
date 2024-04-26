2a10ff8772c6231c4eb60cf9a96a8cdfb9152f54
LittleFox4112 <quinelawensky@gmail.com>
2024-04-26 12:17:08 +0700

    file transisi

--- Transisi Kamar.swift
+++ Transisi Kamar.swift
@@ -1,15 +1,48 @@
 //
-//  Transisi Kamar.swift
-//  Loti
+//  Tes transisi.swift
+//  cadoptio
 //
 //  Created by Quinela Wensky on 26/04/24.
 //
 
 import SwiftUI
 
+struct Gambar {
+    var awal: String = "sceneBnW"
+    var akhir: String = "sceneBiru"
+
+}
+    
 struct Transisi_Kamar: View {
+    var currentImage: Gambar = Gambar()
+    @State var animate: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Image(currentImage.awal)
+            Image(currentImage.akhir)
+                .mask({
+                    Circle()
+                        .frame(width: self.animate ? 1200 : 10, height: self.animate ? 1200 : 10)
+                        .position(CGPoint(x: 90.0, y: 350.0))
+                        .blur(radius: 20)
+                })
+                .opacity(animate ? 1 : 0)
+            
+//            Circle()
+//                .frame(width: self.animate ? 1200 : 10, height: self.animate ? 1200 : 10)
+//                .position(CGPoint(x: 90.0, y: 350.0))
+//                .blur(radius: 10)
+            
+            Image("Hp")
+                .onTapGesture {
+                    withAnimation(Animation.easeInOut(duration: 1)) {
+                        animate.toggle()
+                    }
+                }
+                .position(CGPoint(x: 90.0, y: 350.0))
+        }
+        .ignoresSafeArea()
     }
 }
 
