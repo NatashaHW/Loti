7c0170564f620a2da012c904216258a8d14b69d7
LittleFox4112 <quinelawensky@gmail.com>
2024-04-30 00:27:38 +0700

    Drag and drop aset ide Scene 3 Diskusi

---
+++ Loti/Scene3_Ide.swift
@@ -0,0 +1,8 @@
+//
+//  Scene3_Ide.swift
+//  Loti
+//
+//  Created by Quinela Wensky on 29/04/24.
+//
+
+import Foundation
---
+++ Loti/TesScene3Diskusi.swift
@@ -0,0 +1,18 @@
+//
+//  TesScene3Diskusi.swift
+//  Loti
+//
+//  Created by Quinela Wensky on 29/04/24.
+//
+
+import SwiftUI
+
+struct TesScene3Diskusi: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TesScene3Diskusi()
+}
